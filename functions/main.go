56730d9c3827bf93e108638bcfe3fbdab0929852
Jordan Glean Veitia <jordanglean@outlook.com>
2025-12-14 11:38:59 +0000

    functions type

---
+++ functions/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println("Number: ", numbers)
+	fmt.Println("Doubled Number: ", doubled)
+	fmt.Println("Tripled Number: ", tripled)
+}
+
+func transformNumbers(number *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+	for _, value := range *number {
+		dNumbers = append(dNumbers, transform(value))
+	}
+	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
