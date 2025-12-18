747f1628420d7d8415703a2b06cfa36a2ce92eb2
Jordan Glean Veitia <jordanglean@outlook.com>
2025-12-18 14:04:12 +0000

    add db connection

--- rest-api/models/event.go
+++ rest-api/models/event.go
@@ -1,1 +1,23 @@
 package models
+
+import "time"
+
+type Event struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Location    string    `json:"location" binding:"required"`
+	DateTime    time.Time `json:"date" binding:"required"`
+	UserID      int       `json:"user_id"`
+}
+
+var events = []Event{}
+
+func (e *Event) Save() {
+	// add to db
+	events = append(events, *e)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
