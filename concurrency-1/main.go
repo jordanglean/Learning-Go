2fe15abcd4da3ede79aebb825cda293595e8b518
Jordan Glean Veitia <jordanglean@outlook.com>
2025-12-18 12:32:31 +0000

    wip

--- concurrency-1/main.go
+++ concurrency-1/main.go
@@ -14,21 +14,16 @@ func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan)
 }
 
 func main() {
-	dones := make([]chan bool, 4)
-	dones[0] = make(chan bool)
-	dones[1] = make(chan bool)
-	dones[2] = make(chan bool)
-	dones[3] = make(chan bool)
+	done := make(chan bool)
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
+	go slowGreet("How ... are ... you ...?", done)
+	go greet("I hope you're liking the course!", done)
 
-	go greet("Nice to meet you!", dones[0])
-	go greet("How are you?", dones[1])
-	go slowGreet("How ... are ... you ...?", dones[2])
-	go greet("I hope you're liking the course!", dones[3])
-
-	for _, channel := range dones {
-		<-channel
+	for range done {
 	}
 }
